4874330f4cc662be892cc32abfd2d243f18bc7db
EnWaffel <89415364+EnWaffel@users.noreply.github.com>
2025-02-10 13:56:51 +0100

    Replaced the CMake file with setup scripts and CMake files for linux and windows

---
+++ setup.bat
@@ -0,0 +1,14 @@
+@echo off
+
+cls
+echo Making things ready for you...
+echo.
+
+cd depend
+git clone https://github.com/glfw/glfw.git
+git clone https://github.com/kcat/openal-soft.git
+git clone https://github.com/assimp/assimp.git
+cd ..
+copy scripts\\CMakeLists.txt.win CMakeLists.txt
+
+echo Done :)
